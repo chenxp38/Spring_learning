7d3fed3f415283eb754208b56a9685b86211c41a
chenxp38 <1160522425@qq.com>
2019-12-10 20:29:59 +0800

    返回简单封装Json的Demo

---
+++ src/main/java/com/example/demo/Controller/UserContoller.java
@@ -0,0 +1,45 @@
+package com.example.demo.Controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.pojo.JSONResult;
+import com.example.demo.pojo.User;
+
+//@Controller
+@RestController		// @RestController = @Controller + @ResponseBody
+@RequestMapping("/user")
+public class UserContoller {
+
+	@RequestMapping("/getUser")
+//	@ResponseBody
+	public User getUser() {
+		
+		User u = new User();
+		u.setName("chen");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setPassword("cxp");
+		u.setDesc("getUser");
+		
+		return u;
+	}
+	
+	@RequestMapping("/getUserJson")
+//	@ResponseBody
+	public JSONResult getUserJson() {
+		
+		User u = new User();
+		u.setName("chen1");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setPassword("cxp1");
+		u.setDesc("getUserJson");
+		
+		return JSONResult.ok(u);
+	}
+}
