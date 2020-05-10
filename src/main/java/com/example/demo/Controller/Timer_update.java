08065b612c35abe0dfe92fb73599193033bf707b
chenxp38 <1160522425@qq.com>
2020-05-11 00:18:43 +0800

    库存的重置出现异常，现在进行测试

--- src/main/java/com/example/demo/Controller/Timer_update.java
+++ src/main/java/com/example/demo/Controller/Timer_update.java
@@ -21,7 +21,7 @@ public class Timer_update{
         int time = getRestTime();
         Timer timer = new Timer();//单位是毫秒
         //timer.schedule(new MyTask(), time, 1000*60*60*24);//在time秒后执行此任务,每次间隔24 hours执行一次.
-        timer.schedule(new MyTask(), time, 1000*60);//test
+        timer.schedule(new MyTask(), 1000, 1000*60);//test
         while(true){//这个是用来停止此任务的,否则就一直循环执行此任务
             int count = 0;
             if(count > 0){
